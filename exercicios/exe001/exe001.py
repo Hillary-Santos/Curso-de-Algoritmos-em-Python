8c447757fe84d6000e9348635ccca10b9e314ef6
Hillary Santos <95984325+Hillary-Santos@users.noreply.github.com>
2022-03-12 14:56:54 -0300

    Add files via upload

---
+++ exercicios/exe001/exe001.py
@@ -0,0 +1,6 @@
+idade = input('Digite sua idade: ')
+idade = int(idade)
+
+maior_de_idade = idade == 18 or idade > 18
+
+print(f'Você é maior de idade: {maior_de_idade}')
---
+++ exercicios/exe002/exe002.py
@@ -0,0 +1,6 @@
+num = input('Digite um número: ')
+num = int(num)
+
+parOuImpar = num % 2 == 0
+
+print(f'O número {num} é par: {parOuImpar}')
